e80d20d636235754fcc1695ce976a42a3f1354c8
haseebmir07 <haseebrecovery393@gmail.com>
2026-03-03 00:09:36 +0530

    remove config submodule

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
 import WhatsAppSticky from "@/components/WhatsAppSticky";
-import Script from "next/script"; // ✅ ADD THIS
+import Script from "next/script";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -20,9 +20,8 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${outfit.className} antialiased text-gray-700`}
-        >
+        <body className={`${outfit.className} antialiased text-gray-700`}>
+          
           <Toaster />
 
           <AppContextProvider>
@@ -32,23 +31,21 @@ export default function RootLayout({ children }) {
             <WhatsAppSticky />
           </AppContextProvider>
 
-       
-
-
-
-        
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/69a5d666b0555e1c3f0bacc3/1jinsr0l7';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-
+          {/* ✅ Updated Tawk.to Chatbot Script */}
+          <Script id="tawk-chat" strategy="afterInteractive">
+            {`
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),
+                    s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/69a5d666b0555e1c3f0bacc3/1jinsr0l7';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `}
+          </Script>
 
         </body>
       </html>
